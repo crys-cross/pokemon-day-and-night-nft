d73a633f860c902dc277e1654cdfb9433606c370
CRYS <mariano.christopher123@gmail.com>
2022-07-02 22:19:29 +0800

    Start work for contract

---
+++ contracts/Catch.sol
@@ -0,0 +1,43 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.8;
+
+import "@chainlink/contracts/src/v0.8/interfaces/VRFCoordinatorV2Interface.sol";
+import "@chainlink/contracts/src/v0.8/VRFConsumerBaseV2.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+error RandomIpfsNft__RangeOutOfBounds();
+
+contract Catch is VRFConsumerBaseV2, ERC721URIStorage, Ownable {
+    // Type Declaration
+    enum DayOrNight {
+        Day,
+        Night
+    }
+
+    // Chainlink VRF Variables
+    VRFCoordinatorV2Interface private immutable i_vrfCoordinator;
+    uint64 private immutable i_subscriptionId;
+    bytes32 private immutable i_gasLane;
+    uint32 private immutable i_callbackGasLimit;
+    uint16 private constant REQUEST_CONFIRMATIONS = 3;
+    uint32 private constant NUM_WORDS = 1;
+
+    // VRF Helpers
+    mapping(uint256 => address) public s_requestIdToSender;
+
+    // NFT Variables
+    uint256 public s_tokenCounter;
+    uint256 internal constant MAX_CHANCE_VALUE = 100;
+    uint256 internal immutable i_mintFee;
+
+    // Events
+    event NftRequested(uint256 indexed requestId, address requester);
+
+    constructor() VRFConsumerBaseV2() ERC721() {}
+
+    function mintDay() private {}
+
+    function mintNight() private {}
+}
