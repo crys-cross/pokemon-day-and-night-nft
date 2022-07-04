70c2a069017fe7f783b05c24935842393b0690f4
CRYS <mariano.christopher123@gmail.com>
2022-07-04 22:43:44 +0800

    Edit test file

---
+++ test/unit/catch.test.js
@@ -0,0 +1,9 @@
+const { assert, expect } = require("chai")
+const { getNamedAccounts, deployments, ethers, network } = require("hardhat")
+const { developmentChains, networkConfig } = require("../../helper-hardhat-config")
+
+!developmentChains.includes(network.name)
+    ? describe.skip
+    : describe("Raffle Unit Test", () => {
+          let raffle, VRFCoordinatorV2Mock, raffleEntranceFee, deployer, interval
+          const chainId = network.config.chainId
