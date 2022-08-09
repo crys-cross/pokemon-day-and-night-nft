80eee6775ddadf2f6a182c0fd43d25b8ed63210d
CRYS <mariano.christopher123@gmail.com>
2022-08-09 09:44:05 +0800

    added draft deploy scripts with metadata

---
+++ deploy/01-deploy-catch-nft.ts
@@ -0,0 +1,112 @@
+import {
+    developmentChains,
+    networkConfig,
+    VERIFICATION_BLOCK_CONFIRMATIONS,
+} from "../helper-hardhat-config"
+import verify from "../utils/verify"
+import { storeImages, storeTokenUriMetadata } from "../utils/uploadToPinata"
+import { DeployFunction } from "hardhat-deploy/dist/types"
+import { HardhatRuntimeEnvironment } from "hardhat/types"
+
+const FUND_AMOUNT = "1000000000000000000000" //10 LINK ethers.parseUnit
+const imagesLocation = "./images/randomNft"
+const metadataTemplate = {
+    name: "",
+    description: "",
+    image: "",
+    attributes: [
+        {
+            hp: 20,
+            attack: 10,
+            defense: 10,
+            sp_atk: 10,
+            sp_def: 10,
+            speed: 9,
+        },
+    ],
+}
+
+let tokenUris = []
+
+const deployCatchNft: DeployFunction = async (hre: HardhatRuntimeEnvironment) => {
+    const { getNamedAccounts, deployments, network, ethers } = hre
+    const { deploy, log } = deployments
+    const { deployer } = await getNamedAccounts()
+    const chainId = network.config.chainId!
+
+    let vrfCoordinatorV2Address, subscriptionId
+
+    //get the IPFS hashes ofour images(Methods below)
+    //1. With our IPFS node. https://docs.ipfs.io/
+    //2. pinata https://www.pinata.cloud/   pinata-node-sdk
+    //3. nft.storage(uses filecoin network) https://nft.storage/
+    if (process.env.UPLOAD_TO_PINATA == "true") {
+        tokenUris = await handleTokenUris()
+    }
+
+    if (developmentChains.includes(network.name)) {
+        const vrfCoordinatorV2Mock = await ethers.getContract("VRFCoordinatorV2Mock")
+        vrfCoordinatorV2Address = vrfCoordinatorV2Mock.address
+        const tx = await vrfCoordinatorV2Mock.createSubscription()
+        const txReceipt = await tx.wait(1)
+        subscriptionId = txReceipt.events[0].args.subId
+        await vrfCoordinatorV2Mock.fundSubscription(subscriptionId, FUND_AMOUNT)
+    } else {
+        vrfCoordinatorV2Address = networkConfig[chainId].vrfCoordinatorV2
+        subscriptionId = networkConfig[chainId].subscriptionId
+    }
+
+    const waitBlockConfirmations = developmentChains.includes(network.name)
+        ? 1
+        : VERIFICATION_BLOCK_CONFIRMATIONS
+
+    log("----------------------------")
+    // await storeImages(imagesLocation)
+    const args = [
+        vrfCoordinatorV2Address,
+        subscriptionId,
+        networkConfig[chainId]["gasLane"],
+        networkConfig[chainId]["callbackGasLimit"],
+        tokenUris,
+        networkConfig[chainId]["mintFee"],
+    ]
+
+    const randomIpfsNft = await deploy("RandomIpfsNft", {
+        from: deployer,
+        args: args,
+        log: true,
+        waitConfirmations: waitBlockConfirmations || 1,
+    })
+    log("--------------------------------")
+    if (!developmentChains.includes(network.name) && process.env.ETHERSCAN_API_KEY) {
+        log("Verifying...")
+        await verify(randomIpfsNft.address, args)
+    }
+}
+
+const handleTokenUris = async () => {
+    // Check out https://github.com/PatrickAlphaC/nft-mix for a pythonic version of uploading
+    // to the raw IPFS-daemon from https://docs.ipfs.io/how-to/command-line-quick-start/
+    // You could also look at pinata https://www.pinata.cloud/
+    tokenUris = []
+    //store the image in IPFS
+    //store the metadata in IPFS
+    const { responses: imageUploadResponses, files } = await storeImages(imagesLocation)
+    for (const imageUploadResponseIndex in imageUploadResponses) {
+        //create metadata
+        //upload the metadata
+        let tokenUriMetadata = { ...metadataTemplate } //... unpack
+        tokenUriMetadata.name = files[imageUploadResponseIndex].replace(".png", "")
+        tokenUriMetadata.description = `An adorable ${tokenUriMetadata.name} pup!`
+        tokenUriMetadata.image = `ipfs://${imageUploadResponses[imageUploadResponseIndex].IpfsHash}`
+        console.log(`Uploading ${tokenUriMetadata.name}...`)
+        // store the JSON to pinata/IPFS
+        const metadataUploadResponse = await storeTokenUriMetadata(tokenUriMetadata)
+        tokenUris.push(`ipfs://${metadataUploadResponse!.IpfsHash}`)
+    }
+    console.log("Token URIs Uploaded! They are:")
+    console.log(tokenUris)
+    return tokenUris
+}
+export default deployCatchNft
+deployCatchNft.tags = ["all", "randomipfs", "main"]
