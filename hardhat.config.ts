54e211c68b9236d9bb88e81dfc561e8f81da9bc0
CRYS <mariano.christopher123@gmail.com>
2022-08-31 20:40:05 +0800

    updated hardhat

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,15 +1,10 @@
-import "@typechain/hardhat"
-import "@nomiclabs/hardhat-waffle"
-import "@nomiclabs/hardhat-etherscan"
-import "@nomiclabs/hardhat-ethers"
+import "@nomicfoundation/hardhat-toolbox"
 import "hardhat-deploy"
-import "solidity-coverage"
-import "hardhat-gas-reporter"
 import "hardhat-contract-sizer"
 import "dotenv/config"
 import { HardhatUserConfig } from "hardhat/config"
 
-const RINKEBY_RPC_URL = process.env.RINKEBY_RPC_URL
+const GOERLI_RPC_URL = process.env.RINKEBY_RPC_URL
 const PRIVATE_KEY = process.env.PRIVATE_KEY || ""
 const COINMARKETCAP_API_KEY = process.env.COINMARKETCAP_API_KEY
 const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY
@@ -19,12 +14,58 @@ const config: HardhatUserConfig = {
     solidity: {
         compilers: [{ version: "0.8.8" }, { version: "0.8.4" }, { version: "0.6.6" }],
     },
-    defaultNetwork: "hardhat",
     networks: {
-        rinkeby: {
-            url: RINKEBY_RPC_URL || "",
+        hardhat: {
+            // // If you want to do some forking, uncomment this
+            // forking: {
+            //   url: MAINNET_RPC_URL
+            // }
+            chainId: 31337,
+        },
+        goerli: {
+            url: GOERLI_RPC_URL || "",
             accounts: [PRIVATE_KEY],
-            chainId: 4,
+            chainId: 5,
+        },
+        fantomtest: {
+            url: "https://rpc.testnet.fantom.network" || "",
+            accounts: [PRIVATE_KEY],
+            chainId: 4002,
+        },
+        fujitest: {
+            url: "https://api.avax-test.network/ext/bc/C/rpc" || "",
+            accounts: [PRIVATE_KEY],
+            chainId: 43113,
+        },
+        mumbaitest: {
+            url: "https://polygon-testnet.public.blastapi.io" || "",
+            accounts: [PRIVATE_KEY],
+            chainId: 80001,
+        },
+        optimismkovan: {
+            url: "https://kovan.optimism.io/" || "",
+            accounts: [PRIVATE_KEY],
+            chainId: 69,
+        },
+        bnbtest: {
+            url: "https://data-seed-prebsc-1-s1.binance.org:8545" || "",
+            accounts: [PRIVATE_KEY],
+            chainId: 97,
+        },
+        // arbitrum: {
+        //     url: "https://data-seed-prebsc-1-s1.binance.org:8545" || "",
+        //     accounts: [PRIVATE_KEY],
+        //     chainId: 97,
+        // },
+        metisstardust: {
+            url: "https://stardust.metis.io/?owner=588" || "",
+            accounts: [PRIVATE_KEY],
+            chainId: 588,
+        },
+        evmostest: {
+            url: "https://eth.bd.evmos.dev:8545" || "",
+            accounts: [PRIVATE_KEY],
+            chainId: 9000,
         },
     },
     gasReporter: {
